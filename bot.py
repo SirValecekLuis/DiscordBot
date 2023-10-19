ba90bbce89df1b42320fd2cd023c96bb84f6de4e
Astra3 <rtaborsky02@gmail.com>
2023-10-19 22:32:59 +0200

    adding a linter and requirements.txt

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ client = discord.Client(intents=intents)
 
 # setup on_ready listener
 @client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
+async def on_ready() -> None:
+    print(f"Logged in as {client.user}")
 
 
 # setup on_message event listener
 @client.event
-async def on_message(message):
-    print(f'{message.author}: {message.content}')
+async def on_message(message: discord.Message) -> None:
+    print(f"{message.author}: {message.content}")
--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-import discord
 import json
+import sys
 
 # import client from bot, bot.py sets up the client
 from bot import client
 
 # try to load the config file into 'conf' dictionary
-confFileName = "Data/conf.json"
+# TODO would probably be ideal to load from pydantic settings or something like that
+CONF_FILENAME = "Data/conf.json"
 try:
-    with open(confFileName, 'r') as confFile:
-        conf = json.load(confFile)
+    with open(CONF_FILENAME) as f:
+        conf = json.load(f)
 
 # handle a case where the file doesn't exist
 
 except FileNotFoundError:
-    print(f"File {confFileName} doesn't exist, you need to create it first!")
-    exit()
+    print(f"File {CONF_FILENAME} doesn't exist, you need to create it first!")
+    sys.exit()
 
 # handle any error that could've occured
 except Exception as e:
-    print(f"An error has occured: {str(e)}")
+    print(f"An error has occurred: {e}")
 
 # check if there is a token set in the config
 if "Token" not in conf:
-    print("You need to set your discord bot token in Data/conf.json")
-    print("Refer to documentation for further instructions")
-    exit()
+    print("You need to set your discord bot token in Data/conf.json\n"
+          "Refer to documentation for further instructions")
+    sys.exit()
 
 
 # run discord client
