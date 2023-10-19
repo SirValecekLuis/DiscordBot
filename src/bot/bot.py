9955de77818cacf9025e4fb7a0fe7c0a16eed132
Ivo Slováček <ivoslovacek@gmail.com>
2023-10-19 23:11:26 +0200

    Reworked code structure

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,19 +1,19 @@
 import discord
 
 # setup discord client
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
 
+# create discord client class instance
 client = discord.Client(intents=intents)
 
 
 # setup on_ready listener
-@client.event
+@ client.event
 async def on_ready() -> None:
     print(f"Logged in as {client.user}")
 
 
 # setup on_message event listener
-@client.event
+@ client.event
 async def on_message(message: discord.Message) -> None:
     print(f"{message.author}: {message.content}")
