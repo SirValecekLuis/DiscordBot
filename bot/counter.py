54e0eb05eec481418f1942d04782743a876fbf5a
Tobias <toby.janca@seznam.cz>
2023-10-21 18:13:57 +0200

    changed to async & added function to count poli words

--- bot/counter.py
+++ bot/counter.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from start import db
 
 """
-There is no issue to add new counter, the function add_to_counter is made to add a counter to user if the user has
+There is no issue to add a new counter, the function add_to_counter is made to add a counter to user if the user has
 already a record in the database but without the specified counter. Then just simply fill the arguments which
 are needed for add_to_counter func.
 """
@@ -13,8 +13,10 @@ COUNTERS = [
     "counter_poli",
 ]
 
-# TODO: REWRITE NA ASYNC
-def add_to_counter(user_id: int, count: int, counter: str) -> None:
+poli_words = ["poli", "polim", "poliho", "polimu"]
+
+
+async def add_to_counter(user_id: int, count: int, counter: str) -> None:
     """
     Function goes through all users and if user has a record, +1 is added to specific counter, if record is not
     found a new record is created.
@@ -36,11 +38,27 @@ def add_to_counter(user_id: int, count: int, counter: str) -> None:
     else:
         # If user was not found, I will create a new one + add all counters
         db.Counter.insert_one({"id": user_id})
-        for counter_from_list in COUNTERS:  # Going through all counters
+        for counter_from_list in COUNTERS:
             if counter_from_list == counter:  # If I find the same counter as was specified, I add count to counter
                 db.Counter.update_one({"id": user_id}, {"$set": {counter_from_list: count}})
             else:  # Else I set counter to 0 bud it is nice to have all records set to 0 immediately
                 db.Counter.update_one({"id": user_id}, {"$set": {counter_from_list: 0}})
+
+
+async def count_words(message: str, words: list) -> int:
+    """
+    this function is supposed how many specified words are in given message.
+    :param message: text from discord message
+    :param words: list of words which are to be found and counted from message
+    :return: amount of found words
+    """
+    count = 0
+    message = message.split()  # It needs to be split due to finding specific words
+
+    for word in words:
+        count += message.count(word)
+
+    return count
 
 
 class Counter(commands.Cog):
@@ -49,15 +67,14 @@ class Counter(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        """ This part needs to be reworked in the future, not the best way to do this """
         lowered_message = message.content.lower()
         tobias_count = lowered_message.count("tobiáš")
-        poli_count = lowered_message.split().count("poli") + lowered_message.count("olivk")
+        poli_count = await count_words(lowered_message, poli_words) + lowered_message.count("olivk")
 
         if tobias_count > 0:
-            add_to_counter(message.author.id, tobias_count, "counter_tobias")
+            await add_to_counter(message.author.id, tobias_count, "counter_tobias")
         if poli_count > 0:
-            add_to_counter(message.author.id, poli_count, "counter_poli")
+            await add_to_counter(message.author.id, poli_count, "counter_poli")
 
 
 def setup(bot: discord.Bot) -> None:
