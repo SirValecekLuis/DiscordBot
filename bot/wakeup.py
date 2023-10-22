e201c356091627865df348da874bb71616c938fe
standa <st.urban27@gmail.com>
2023-10-22 17:34:09 +0200

    added cooldown

--- bot/wakeup.py
+++ bot/wakeup.py
@@ -2,23 +2,30 @@ import asyncio
 import discord
 from discord.ext import commands
 
+
 class WakeUp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.slash_command(
-        name="wakeup",
-        description="Ping a user multiple times and delete the messages."
+        name="wakeup", description="Ping a user multiple times and delete the messages."
     )
+    @commands.cooldown(2, 86400)  # Allow 2 uses per day (86400 seconds in a day)
     async def wakeup(self, ctx, user: discord.User):
+        if ctx.command.get_cooldown_retry_after(ctx) > 0:
+            await ctx.respond(
+                f"Máš cooldown. Prosím počkej {ctx.command.get_cooldown_retry_after(ctx)/3600:.2f} hod před použitím příkazu znovu.",
+                ephemeral=True,
+            )
+            return
         # loops
         loops = 3
         loop_delay = 2
 
         # Define the number of pings in a row in 1 loop
-        num_pings = 5 # going for more results in getting blocked by discord anti-spam, hence the loop mechanism - even more annoying
+        num_pings = 5  # Going for more results in getting blocked by Discord anti-spam
 
-        # Reply so discord doesn't throw an error
+        # Reply so Discord doesn't throw an error
         await ctx.respond("Začínám pingovat", ephemeral=True)
 
         for i in range(loops):
@@ -31,13 +38,11 @@ class WakeUp(commands.Cog):
             for message in ping_messages:
                 await asyncio.sleep(0.1)  # Adjust the sleep duration as needed
                 await message.delete()
-            if (i != loops-1):
+            if i != loops - 1:
                 await asyncio.sleep(loop_delay)
 
         await ctx.respond("Končím s pingováním", ephemeral=True)
-        
 
-        
 
 def setup(bot):
     bot.add_cog(WakeUp(bot))
