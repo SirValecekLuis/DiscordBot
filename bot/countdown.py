6d7b0c6134f5f65e0d06344ee4002412b76158f2
Tobias <toby.janca@seznam.cz>
2024-05-19 13:30:10 +0200

    Changed message to show minutes and seconds as well + supports czech plural and singular now

--- bot/countdown.py
+++ bot/countdown.py
@@ -14,11 +14,13 @@ class CountDown(commands.Cog):
     async def on_ready(self) -> None:
         pass
 
-    @tasks.loop(hours=1)
+    @tasks.loop(hours=1, minutes=1, seconds=1)
     async def start_countdown_called(self):
         if self.message_to_delete is not None:
             await self.message_to_delete.delete()
 
+        # Test 1164923191542693920
+        # Ofiko 1078965904093745252
         channel = self.bot.get_channel(1078965904093745252)  # Hardcoded variable is the best variable
         time_now_utc = datetime.now(timezone.utc)
 
@@ -32,15 +34,27 @@ class CountDown(commands.Cog):
         time_now = time_now_utc.astimezone(prague_tz)
         time_exam = datetime(2024, 5, 30, 8, 0, tzinfo=prague_tz)
         time_diff = time_exam - time_now
-        time_diff = round(time_diff.total_seconds() / 3600)
 
-        if time_diff < 0:
+        if time_diff.total_seconds() < 0:
             return
+
+        total_seconds = int(time_diff.total_seconds())
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+
+        def pluralize(value, singular, plural):
+            return singular if value == 1 else plural
+
+        hours_str = pluralize(hours, "hodinu", "hodiny" if hours in [2, 3, 4] else "hodin")
+        minutes_str = pluralize(minutes, "minutu", "minuty" if minutes in [2, 3, 4] else "minut")
+        seconds_str = pluralize(seconds, "sekundu", "sekundy" if seconds in [2, 3, 4] else "sekund")
 
         guild = self.ctx.guild
         emoji = discord.utils.get(guild.emojis, name="olivkacursed")
 
-        self.message_to_delete = await channel.send(f"*Čas do zkoušky zbývá: {time_diff} hodin.* {emoji}")
+        self.message_to_delete = await channel.send(
+            f"***Tvá smrt v podobě zkoušky z APPS přichází za {hours} {hours_str} {minutes} {minutes_str} a {seconds} {seconds_str}.*** {emoji}\n"
+            f"*||Zlé hlasy říkají, že úspěšnost je 50%. :skull:||*")
 
     @commands.slash_command(name='start_countdown')
     async def start_countdown(self, ctx: discord.ApplicationContext):
