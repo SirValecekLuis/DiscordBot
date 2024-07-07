417ef6f542a40bbef98660cf472adbb646723bcd
Tobias <toby.janca@seznam.cz>
2024-07-07 10:34:18 +0200

    Switching logic done with roles and perms changed

--- bot/semester_switching.py
+++ bot/semester_switching.py
@@ -1,4 +1,4 @@
-"""This module is for semestr switching, wow."""
+"""This module is for semester switching, wow."""
 import discord
 from discord.ext import commands
 import math
@@ -18,19 +18,30 @@ class SemesterSwitching(commands.Cog):
     async def switch_semester(self, ctx: discord.ApplicationContext) -> None:
         """
         PREREQUISITES
-        channel name is in this way: "x. semestr" or "x. semestr - archiv"
-        discord roles will follow the same pattern as "EMOJI NUMBER NAME" such as "1️⃣ Prvák" and will not be changed
+        channel name is in this way: "x. semestr" or "x. semestr - archiv" and only 0-9 is supported right now.
+        discord roles will follow the same pattern as "EMOJI NUMBER NAME" such as "1️⃣ Prvák" and will not be changed.
         """
         # TODO: test what happens if someone calls this without ADMIN perms
+        # TODO: add some level of stupid proofing (maybe some kind of acceptance from others or something like that)
         try:
+            await ctx.send("Please, bear in mind this command may take several minutes due to discord API calls.",
+                           delete_after=True)
             await ctx.defer(ephemeral=True)  # Discord API takes way too long to respond
 
+            # This is just for error purposes
+            visited_categories = []
             for category in ctx.guild.categories:
+                print(category)
                 category_name = category.name.split()
                 if len(category_name[0]) == 2 and category_name[1] == "semestr" and len(category_name) == 2:
-                    # Semester is active and should be put as archive
+                    # Semester is active and should be put as archived
 
-                    # Removes role (prvák, druhák...)
+                    """
+                    Adds - archive to a category name
+                    Removes role (prvák, druhák...)
+                    Disallows public/private threads, send messages in them and sending messages overall for everyone
+                    Resets View Channel for everyone to its default state
+                    """
                     new_overwrites = category.overwrites
                     for role_name, overwrite in category.overwrites.items():
                         year = math.ceil(int(category_name[0][0]) / 2)  # [1]. semestr = 1/2 = ceil(0.5) = 1. year
@@ -38,6 +49,13 @@ class SemesterSwitching(commands.Cog):
                             role_to_remove = discord.utils.get(category.guild.roles, name=ROLES[year])
                             new_overwrites = {role: overwrite for role, overwrite in category.overwrites.items() if
                                               role != role_to_remove}
+                    new_overwrites[category.guild.default_role] = discord.PermissionOverwrite(
+                        create_public_threads=False,
+                        create_private_threads=False,
+                        send_messages_in_threads=False,
+                        send_messages=False,
+                        view_channel=None
+                    )
 
                     # Name of channel
                     name = category.name + " - archiv"
@@ -45,9 +63,28 @@ class SemesterSwitching(commands.Cog):
                     print(name)
 
                     # Final edit of category
+                    visited_categories.append(category)
                     await category.edit(name=name, overwrites=new_overwrites)
-                elif len(category_name[0]) == 2 and category_name[1] == "semestr" and len(category_name) > 2:
+                elif len(category_name[0]) == 2 and category_name[1] == "semestr" and category_name[3] == "archiv":
                     # Semester is archived and should be put as active
+
+                    """
+                    Removes - archive from name
+                    Adds role (prvák, druhák...) and enables View Channel for the role
+                    Disallows view channel for everyone
+                    Allows Send Messages, Send Messages in Threads and Create Private/Public threads for everyone
+                    """
+                    new_overwrites = category.overwrites
+                    year = math.ceil(int(category_name[0][0]) / 2)  # [1]. semestr = 1/2 = ceil(0.5) = 1. year
+                    role_to_add = discord.utils.get(category.guild.roles, name=ROLES[year])
+                    new_overwrites[role_to_add] = discord.PermissionOverwrite(view_channel=True)
+                    new_overwrites[category.guild.default_role] = discord.PermissionOverwrite(
+                        view_channel=False,
+                        create_public_threads=True,
+                        create_private_threads=True,
+                        send_messages_in_threads=True,
+                        send_messages=True
+                    )
 
                     # Name of channel
                     name = category_name[0] + " " + category_name[1]
@@ -55,11 +92,15 @@ class SemesterSwitching(commands.Cog):
                     print(name)
 
                     # Final edit of category
-                    await category.edit(name=name)
+                    visited_categories.append(category)
+                    await category.edit(name=name, overwrites=new_overwrites)
 
             await ctx.followup.send("Channels switched!", ephemeral=True)
         except discord.HTTPException as e:
-            raise Exception("semester_switching failed. 15m API response was not enough." + e)
+            text = " | ".join([category.name for category in visited_categories])
+            raise Exception("semester_switching failed. 15m API response was not enough. In this case, unfortunately,"
+                            "we have no idea what was executed and what was not. Therefore there is a list of"
+                            "categories that have been/could have been changed with error attached." + text + e)
 
     async def cog_command_error(self, ctx: discord.ApplicationContext, error: commands.CommandError) -> None:
         await send_error_message_to_user(ctx, error)
