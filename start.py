e1c66917235ffa2b77d1b155081136b306e29918
Tobias <toby.janca@seznam.cz>
2024-07-11 15:54:23 +0200

    Now loads all cogs from folder "bot" automatically

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 """Script to start the bot."""
+
+import os
 import discord
 from discord.ext import commands
 
@@ -14,24 +16,44 @@ bot = commands.Bot(intents=intents)
 # using Database class from database.py
 db = Database(settings.database_login)
 
-# list of all cog modules
-cog_list = [
-    "auto_pin",
-    "auto_voice",
-    "counter",
-    "leaderboard",
-    "login_info",
-    "welcome_message",
-    # "countdown", - This module is for now removed as it is not useful
-    "semester_switching",
-    "database_communication",
-    "help",
-]
+def load_cogs() -> None:
+    """
+    This function will load all the cogs based on the files from bot folder.
+    :return: None
+    """
 
-# load every cog in cog_list
-for cog in cog_list:
-    bot.load_extension(f"bot.{cog}")
+    current_file = os.path.abspath(__file__)
 
-# run discord client with discord token from .env variable "TOKEN"
+    # Get the current file path
+    current_dir = os.path.dirname(current_file)
+
+    # "bot" folder path
+    bot_dir = os.path.join(current_dir, 'bot')
+
+    # Checks if "bot" folder exists
+    if not os.path.exists(bot_dir):
+        print("Složka 'bot' nebyla nalezena.")
+        return
+
+    # Get all python files in folder "bot"
+    python_files = [file for file in os.listdir(bot_dir) if
+                    file.endswith('.py') and os.path.isfile(os.path.join(bot_dir, file))]
+
+    # Get names without .py
+    cog_list = [os.path.splitext(file)[0] for file in python_files]
+
+    # Remove __init__ as it is useless and it would cause an error
+    try:
+        cog_list.remove("__init__")
+    except ValueError:
+        ...
+
+    # Load every cog in cog_list
+    for cog in cog_list:
+        bot.load_extension(f"bot.{cog}")
+
+
+# Run discord client with discord token from .env variable "TOKEN"
 if __name__ == "__main__":
+    load_cogs()
     bot.run(settings.bot_token)
